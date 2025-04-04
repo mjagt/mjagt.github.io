f1281662d536e26f61b9199fe09f1dc2a08e58b9
Mark Jagt <freakingdutch@gmail.com>
2025-04-04 12:29:28 +0200

    Added dbFunctions

---
+++ dbFunctions.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+function readDb(dbName = 'db.json') {
+    const data = fs.readFileSync(dbName, 'utf8')
+    return JSON.parse(data)
+}
+
+function writeDb(obj, dbName = 'db.json') {
+    if(!obj) return console.log('Please provide data to save')
+    try {
+        fs.writeFileSync(dbName, JSON.stringify(obj))
+        return console.log('SAVE SUCCESS')
+    } catch {
+        return console.log('WRITE FAILED')
+    }
+}
+
+module.exports = { readDb, writeDb}
